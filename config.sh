9bf09673481d145aaf53acc31861e47c97832035
motoonepower <101825685+motoonepower@users.noreply.github.com>
2022-05-01 09:12:36 +0530

    11

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Devic
-export FOX_BRANCH="fox_10.0"
-export DT_LINK="https://github.com/rushiranpise/android_device_motorola_chef_twrp -b retrofit-test"
+export FOX_BRANCH="fox_11.0"
+export DT_LINK="https://github.com/rushiranpise/android_device_motorola_chef_twrp -b retrofit"
 
 export DEVICE="chef"
 export OEM="motorola"
@@ -10,7 +10,7 @@ export TARGET="recoveryimage"
 export OUTPUT="OrangeFox*.zip"
 # Kernel Source
 # Uncomment the next line if you want to clone a kernel source.
-export KERNEL_SOURCE="https://github.com/PixelExperience-Devices/kernel_motorola_msm8998 -b twelve"
+export KERNEL_SOURCE="https://github.com/moto-SDM660/android_kernel_motorola_sdm660 -b twrp-11"
 export PLATFORM="msm8998" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
 
 # Extra Command
